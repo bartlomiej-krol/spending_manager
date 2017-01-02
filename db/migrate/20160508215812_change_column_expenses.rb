4ad002ac4644f287a8b950d0ce341796e79ebcd1
bartlomiej-krol <bartlomiej_krol@interia.pl>
2017-01-02 02:37:15 +0100

    Updated migrations

--- db/migrate/20160508215812_change_column_expenses.rb
+++ db/migrate/20160508215812_change_column_expenses.rb
@@ -1,5 +1,5 @@
 class ChangeColumnExpenses < ActiveRecord::Migration
   def change
-  	change_column :expenses, :repeatable, 'boolean USING CAST(smoking AS boolean)', :default => false
+  	change_column :expenses, :repeatable, 'boolean USING CAST(repeatable AS boolean)', :default => false
   end
 end
