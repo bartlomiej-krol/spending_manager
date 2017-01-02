913bf656e338000de44274661f305a65a71353d0
bartlomiej-krol <bartlomiej_krol@interia.pl>
2017-01-02 04:34:57 +0100

    Updated db on prod

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -7,36 +7,36 @@ class StatisticsController < ApplicationController
 			@total_sum = current_user.expenses.where("status='paid' AND amount > 0.0").sum(:amount)
 			@total_ave = current_user.expenses.where("status='paid' AND amount > 0.0").average(:amount)
 			start = (Date.today - 6.days)
-			@sum7 = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").sum(:amount)
-			@aved7 = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").sum(:amount) / 7
-			@ave7 = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").average(:amount)
+			@sum7 = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").sum(:amount)
+			@aved7 = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").sum(:amount) / 7
+			@ave7 = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").average(:amount)
 			
 			start = Date.today - (Date.today.day - 1).days
-			@sumThisMonth = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").sum(:amount)
-			@avedThisMonth = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").sum(:amount) / Date.today.day
-			@aveThisMonth = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").average(:amount)
+			@sumThisMonth = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").sum(:amount)
+			@avedThisMonth = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").sum(:amount) / Date.today.day
+			@aveThisMonth = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").average(:amount)
 
 			start = (Date.today - 30.days)
-			@sum30 = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").sum(:amount)
-			@aved30 = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").sum(:amount) / 30
-			@ave30 = current_user.expenses.where("status='paid' AND `date` >= '#{start}' AND amount > 0.0").average(:amount)
+			@sum30 = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").sum(:amount)
+			@aved30 = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").sum(:amount) / 30
+			@ave30 = current_user.expenses.where("status='paid' AND date >= '#{start}' AND amount > 0.0").average(:amount)
 		else
 			@total_sum = current_user.expenses.where("status='paid'").sum(:amount)
 			@total_ave = current_user.expenses.where("status='paid'").average(:amount)
 			start = (Date.today - 6.days)
-			@sum7 = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").sum(:amount)
-			@aved7 = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").sum(:amount) / 7
-			@ave7 = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").average(:amount)
+			@sum7 = current_user.expenses.where("status='paid' AND date >= '#{start}'").sum(:amount)
+			@aved7 = current_user.expenses.where("status='paid' AND date >= '#{start}'").sum(:amount) / 7
+			@ave7 = current_user.expenses.where("status='paid' AND date >= '#{start}'").average(:amount)
 			
 			start = Date.today - (Date.today.day - 1).days
-			@sumThisMonth = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").sum(:amount)
-			@avedThisMonth = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").sum(:amount) / Date.today.day
-			@aveThisMonth = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").average(:amount)
+			@sumThisMonth = current_user.expenses.where("status='paid' AND date >= '#{start}'").sum(:amount)
+			@avedThisMonth = current_user.expenses.where("status='paid' AND date >= '#{start}'").sum(:amount) / Date.today.day
+			@aveThisMonth = current_user.expenses.where("status='paid' AND date >= '#{start}'").average(:amount)
 
 			start = (Date.today - 30.days)
-			@sum30 = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").sum(:amount)
-			@aved30 = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").sum(:amount) / 30
-			@ave30 = current_user.expenses.where("status='paid' AND `date` >= '#{start}'").average(:amount)
+			@sum30 = current_user.expenses.where("status='paid' AND date >= '#{start}'").sum(:amount)
+			@aved30 = current_user.expenses.where("status='paid' AND date >= '#{start}'").sum(:amount) / 30
+			@ave30 = current_user.expenses.where("status='paid' AND date >= '#{start}'").average(:amount)
 		end
 	end
 
