868f0d01a58f2585b8cbce04ba2a186aeffbc702
bartlomiej-krol <bartlomiej_krol@interia.pl>
2017-01-02 02:53:24 +0100

    Updated migrations

--- db/migrate/20160516081009_change_column2_expenses.rb
+++ db/migrate/20160516081009_change_column2_expenses.rb
@@ -1,5 +1,5 @@
 class ChangeColumn2Expenses < ActiveRecord::Migration
   def change
-  	change_column :expenses, :repeat_interval, :long, :default => 0
+  	change_column :expenses, :repeat_interval, :bigint, :default => 0
   end
 end
